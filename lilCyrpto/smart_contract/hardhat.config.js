fc2846b22b88255fcecb8f5483bd8a72ffc17309
Erdem Ege Eroğlu <62260171+egeroglu@users.noreply.github.com>
2022-01-31 20:58:12 +0300

    Update hardhat.config.js

--- lilCyrpto/smart_contract/hardhat.config.js
+++ lilCyrpto/smart_contract/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/wBRRYpowRgf0ztQ4FHpj2e6kHEhyPVp4',
-      accounts: ['87afbbbe2381c3a546d42a3a6fb78118c1f45e6145914e68ce6422252f350350'],
+      url: '******************',
+      accounts: ['******************'],
     },
   },
 };
